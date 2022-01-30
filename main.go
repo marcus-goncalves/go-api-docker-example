faa5948d0e2971657f62c496241fb8412fb9bbdf
Marcus Gonçalves <nvrrox@gmail.com>
2022-01-30 17:10:37 -0300

    Update libs

--- main.go
+++ main.go
@@ -1,28 +1,9 @@
 package main
 
-import (
-	"net/http"
-	"os"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-)
+import "go-docker-example/v1/routes"
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+	router := routes.CreateRouter()
 
-	// Routes
-	r.HandleFunc("/", helloWeb)
-
-	PORT := os.Getenv("HTTP_PORT")
-	if PORT == "" {
-		PORT = "3000"
-	}
-
-	http.ListenAndServe(":"+PORT, r)
-}
-
-func helloWeb(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Edu Mano!"))
+	router.Run(":3000")
 }
